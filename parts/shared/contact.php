de956a18731c27a08bf3ef5607ed9772b678365d
Niyo <ngumus14@gmail.com>
2024-05-26 18:09:36 +0200

    Added image files for homepage.

---
+++ parts/shared/contact.php
@@ -0,0 +1,53 @@
+<section data-bs-version="5.1" class="form03 cid-udpc1bDdRF" id="form03-r">
+
+
+
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg item-wrapper">
+                <div class="mbr-section-head mb-5">
+                    <h1 class="mbr-section-title mbr-fonts-style align-center mb-0 "><strong>Kontakt</strong></h1>
+
+                </div>
+                <div class="col-lg-12 mx-auto mbr-form" data-form-type="formoid">
+                    <form action="#" method="POST" class="mbr-form form-with-styler" data-form-title="Form Name"><input type="hidden" name="email" data-form-email="true" value="sYrYSsjqj06y3+6t6qLWeKzwRpukctAu4g5EG4zG90fAINmFiRKzcJ/s4d+Z/TRhoWstl7dAXp0RT8TshUHy4jSnI+1ZQdxelxCRphoWikiU7FW5iflu2KMNyGbIUkQ2">
+                        <div class="row">
+                            <div hidden="hidden" data-form-alert="" class="alert alert-success col-12">Thanks for filling out the form!</div>
+                            <div hidden="hidden" data-form-alert-danger="" class="alert alert-danger col-12">
+                                Oops...! some problem!
+                            </div>
+                        </div>
+                        <div class="dragArea row">
+                            <div class="col-md col-sm-12 form-group mb-3 mb-3" data-for="name">
+                                <input type="text" name="name" placeholder="Name" data-form-field="name" class="form-control" value="" id="name-form03-r">
+                            </div>
+                            <div class="col-md col-sm-12 form-group mb-3 mb-3" data-for="email">
+                                <input type="email" name="email" placeholder="E-mail" data-form-field="email" class="form-control" value="" id="email-form03-r">
+                            </div>
+                            <div class="col-12 form-group mb-3 mb-3" data-for="phone">
+                                <input type="tel" name="phone" placeholder="Telefon" data-form-field="phone" class="form-control" value="" id="phone-form03-r">
+                            </div>
+                            <div class="col-12 form-group mb-3 mb-3" data-for="textarea">
+                                <textarea name="textarea" placeholder="Nachricht" data-form-field="textarea" class="form-control" id="textarea-form03-r"></textarea>
+                            </div>
+                            <div class="col-lg-12 col-md-12 col-sm-12 align-center mbr-section-btn">
+                                <button type="submit" class="btn btn-primary ps-5 pe-5">
+                                    Senden
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="image-wrapper">
+                    <img class="w-100" src="<?php echo get_template_directory_uri(); ?>/images/hand.jpg" alt="Mobirise Website Builder">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
