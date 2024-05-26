a3d4c18cbbd1b78867b1288771519c268cefcec5
Niyo <ngumus14@gmail.com>
2024-05-26 18:05:41 +0200

    about section renewed

--- parts/shared/about.php
+++ parts/shared/about.php
@@ -1,20 +1,28 @@
-<div id="about" class="container-fluid">
-    <div class="container pt-5">
-        <div class="row ">
-            <div class="col-lg-6">
-            <h3 class="text-primary fw-bold">PositiveHouse wurde Anfang 2020 gegründet. Unser Name ist unser Programm.</h3>
+<section data-bs-version="5.1" class="video05 cid-udjIgmCow8" id="video05-3">
+    
+
+    
+    
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="title-wrapper mb-5">
+                <h1 class="mbr-section-maintitle pb-5 fs-1">
+                    <strong>Über uns</strong></h1>
             </div>
-            <ul class="col-lg-6 d-flex flex-column justify-content-around">
-                <li>
-                    <p>Wir entwickeln, produzieren und vertreiben in Europa (zunächst in Deutschland, Frankreich, Benelux und Spanien) spezielle, intelligente und nachhaltige Energiekonzepte.</p>
-                </li>
-                <li>
-                    <p>Unser Ziel ist es, jedem eine deutliche Senkung der Energiekosten in seinem Haus/Gebäude zu ermöglichen und vor allem die Möglichkeit zu bieten, zur Energieautarkie überzugehen. Mit unseren Schlüsselprodukten kann nachweislich fast der gesamte Strombedarf eines durchschnittlichen Hauses (bis zu 150 Quadratmetern) gedeckt werden.</p>
-                </li>
-                <li>
-                    <p>Zusätzliche oder ergänzende Produkte unserer Partner haben ein zusätzliches Energiesparpotenzial und tragen auch dazu bei, Ihre Energiekosten zu senken und den Elektrosmog in Ihrem Haus zu reduzieren. Ein gesünderes Leben ist wirklich möglich.</p>
-                </li>
-            </ul>
+            <div class="col-12 col-md">
+                <div class="text-wrapper">
+                    <h3 class="mb-4 text mbr-section-title text-primary"><strong>PositiveHouse wurde Anfang 2020 gegründet. Unser Name ist unser Programm.</strong></h3>
+                    <p class="mbr-text mb-4  display-7"><strong>
+                        Unser Ziel<em> </em></strong>ist es, <strong>hauptsächlich Genossenschaften</strong>, Verbände und die Industrie mit der Beratung Konsultation und Koordination von Projekten zu begleiten. Indem wir die Planung wie auch die Organisation, von Anfang an bis zum vollendeten Ergebnis durchführen.&nbsp; Mit unseren Schlüsselprodukten kann nachweislich fast der gesamte Strombedarf eines durchschnittlichen Hauses (bis zu 150 Quadratmeter) gedeckt werden
+                    </p>
+                    
+                </div>
+            </div>
+            <div class="col-12 col-md-6 video-block">
+                <div class="mbr-figure"><iframe class="mbr-embedded-video" src="https://www.youtube.com/embed/wrzZRrimy-0?rel=0&amp;amp;mute=1&amp;showinfo=0&amp;autoplay=1&amp;loop=1&amp;playlist=wrzZRrimy-0" width="1280" height="720" frameborder="0" allowfullscreen></iframe></div>
+                
+            </div>
         </div>
     </div>
-</div>
+</section>
